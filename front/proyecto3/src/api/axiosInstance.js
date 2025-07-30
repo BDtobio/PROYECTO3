37d562b06e33dd6bd595f2f01aaf063c94b57872
BDtobio <tobiasbustosdiaco989@gmail.com>
2025-07-30 02:12:32 -0300

    arreglo front

--- front/proyecto3/src/api/axiosInstance.js
+++ front/proyecto3/src/api/axiosInstance.js
@@ -1,6 +1,7 @@
+// src/api/axiosInstance.js (o .ts)
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://dpg-d24pfvili9vc73ehvda0-a.render.com';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
