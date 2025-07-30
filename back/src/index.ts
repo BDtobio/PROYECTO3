62f0d460241714672393c2e57f3a1713b9ae3124
BDtobio <tobiasbustosdiaco989@gmail.com>
2025-07-30 01:58:09 -0300

    commit

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,16 +1,17 @@
 import { AppDataSource } from "./config/appDataSource";
 import server from "./server";
-require ("dotenv").config();
-import "reflect-metadata"
-const PORT=process.env.PORT
+require("dotenv").config();
+import "reflect-metadata";
+
+const PORT = process.env.PORT || 3000;
 
 AppDataSource.initialize()
-    .then(() => {
-        console.log("Database Connection: OK!");
-        server.listen(PORT, () => {
-            console.log(`Server listening on port ${PORT}`);
-        });
-    })
-    .catch(error => {
-        console.error("Error al conectar con la Base de Datos:", error);
+  .then(() => {
+    console.log("Database Connection: OK!");
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
     });
+  })
+  .catch((error) => {
+    console.error("Error al conectar con la Base de Datos:", error);
+  });
--- back/src/server.ts
+++ back/src/server.ts
@@ -1,11 +1,16 @@
-import express  from "express";
-import morgan  from "morgan";
-import cors from "cors"
+import express from "express";
+import  cors  from "cors";
+import  morgan from "morgan";
 import indexRouter from "./routes/indexRouter";
 
-const server=express();
+
+const server=express()
 server.use(cors())
 server.use(express.json())
 server.use(morgan("dev"))
+
 server.use(indexRouter)
-export default server;
+
+
+
+export default server
