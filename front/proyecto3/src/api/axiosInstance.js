b94dc18c206c5c0210c3b2d8daf1017dd1dd5f48
BDtobio <tobiasbustosdiaco989@gmail.com>
2025-08-01 11:40:53 -0300

    commit arreglo

--- front/proyecto3/src/api/axiosInstance.js
+++ front/proyecto3/src/api/axiosInstance.js
@@ -1,7 +1,9 @@
-// src/api/axiosInstance.js (o .ts)
 import axios from 'axios';
 
+// Lee la variable de entorno o usa localhost en desarrollo
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+
+console.log("API URL usada en axios:", API_URL);
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
