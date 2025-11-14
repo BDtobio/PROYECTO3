ed65dde2b4888e5de948c3583fae65f2af3d198a
BDtobio <tobiasbustosdiaco989@gmail.com>
2025-11-14 20:01:19 -0300

    das

--- back/src/config/appDataSource.ts
+++ back/src/config/appDataSource.ts
@@ -1,52 +1,52 @@
-import { DataSource } from "typeorm";
-
-import { DB_DATABASE, DB_DROP, DB_ENTITIES, DB_HOST, DB_LOGGING, DB_PASSWORD, DB_PORT, DB_SYNC, DB_TYPE, DB_USERNAME } from "./envs";
-
-export const AppDataSource = new DataSource({
-    type: DB_TYPE,
-    host: DB_HOST,
-    port: DB_PORT,
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-    synchronize: DB_SYNC,
-    logging: DB_LOGGING,
-    entities: DB_ENTITIES,
-    dropSchema: DB_DROP,
-    
-})
-
 // import { DataSource } from "typeorm";
 
-// import {
-//   DB_DATABASE,
-//   DB_DROP,
-//   DB_ENTITIES,
-//   DB_HOST,
-//   DB_LOGGING,
-//   DB_PASSWORD,
-//   DB_PORT,
-//   DB_SYNC,
-//   DB_TYPE,
-//   DB_USERNAME,
-// } from "./envs";
+// import { DB_DATABASE, DB_DROP, DB_ENTITIES, DB_HOST, DB_LOGGING, DB_PASSWORD, DB_PORT, DB_SYNC, DB_TYPE, DB_USERNAME } from "./envs";
 
 // export const AppDataSource = new DataSource({
-//   type: DB_TYPE,
-//   host: DB_HOST,
-//   port: DB_PORT,
-//   username: DB_USERNAME,
-//   password: DB_PASSWORD,
-//   database: DB_DATABASE,
-//   synchronize: DB_SYNC,
-//   logging: DB_LOGGING,
-//   entities: DB_ENTITIES,
-//   dropSchema: DB_DROP,
+//     type: DB_TYPE,
+//     host: DB_HOST,
+//     port: DB_PORT,
+//     username: DB_USERNAME,
+//     password: DB_PASSWORD,
+//     database: DB_DATABASE,
+//     synchronize: DB_SYNC,
+//     logging: DB_LOGGING,
+//     entities: DB_ENTITIES,
+//     dropSchema: DB_DROP,
+    
+// })
 
-//   ssl: true, // 👈 Importante para Render
-//   extra: {
-//     ssl: {
-//       rejectUnauthorized: false, // 👈 Necesario si Render usa certificados no verificados
-//     },
-//   },
-// });
+import { DataSource } from "typeorm";
+
+import {
+  DB_DATABASE,
+  DB_DROP,
+  DB_ENTITIES,
+  DB_HOST,
+  DB_LOGGING,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_SYNC,
+  DB_TYPE,
+  DB_USERNAME,
+} from "./envs";
+
+export const AppDataSource = new DataSource({
+  type: DB_TYPE,
+  host: DB_HOST,
+  port: DB_PORT,
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  synchronize: DB_SYNC,
+  logging: DB_LOGGING,
+  entities: DB_ENTITIES,
+  dropSchema: DB_DROP,
+
+  ssl: true, // 👈 Importante para Render
+  extra: {
+    ssl: {
+      rejectUnauthorized: false, // 👈 Necesario si Render usa certificados no verificados
+    },
+  },
+});
